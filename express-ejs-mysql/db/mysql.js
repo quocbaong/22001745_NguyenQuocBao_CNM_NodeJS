e169e9d3835bc46456c2e17c9c55d73a17af7ebe
quocbaong <nbao76296@gmail.com>
2026-01-15 10:38:19 +0700

    Lab 1 - Add Product

---
+++ express-ejs-mysql/db/mysql.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'shopdb'
+});
+
+module.exports = pool.promise();
